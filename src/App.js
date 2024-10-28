e685f3934dcd4b527a0c512a271c8ccb7bdee79a
jiwon <wldnjs4685@gmail.com>
2024-10-28 13:10:24 +0900

    add date, vot av

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ function Home(){
             poster_path={movie.poster_path}
             title={movie.title}
             overview={movie.overview}
-            genre_ids={movie.genre_ids}
-            id={movie.id}
+            release_date={movie.release_date}
+            vote_average={movie.vote_average}
           />
         </div>
       ))}
--- src/movie/movie.js
+++ src/movie/movie.js
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-function Movie({poster_path, title, overview, genre_ids, id}) {
+function Movie({poster_path, title, overview, genre_ids, id, vote_average, release_date}) {
     return (
         <div>
             <img src={"https://image.tmdb.org/t/p/w200" + poster_path} alt={title}/>
             <h2>{title}</h2>
             <p>{overview}</p>
-            <ul>{genre_ids.map((genre) => (<li key={genre}>{genre}</li>))}</ul>
+            <p>평점 : {vote_average}</p>
+            <p>개봉일 : {release_date}</p>
         </div>
     );
 }
